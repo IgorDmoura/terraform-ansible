8da108169c69f62c9b4c54d23f989f3a17e4510d
Igor Moura <igor.dmoura01@gmail.com>
2024-03-23 14:05:05 -0300

    Add project to github

---
+++ main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0eb199b995e2bc4e3"
+  instance_type = "t2.micro"
+  key_name = "iac-key"
+  # user_data = <<-EOF
+  # #!/bin/bash
+  # cd /home/ubuntu
+  # echo "<h1> Feito com Terraform</h1>" > index.html
+  # nohup busybox httpd -f -p 8080 &
+  # EOF
+
+  tags = {
+    Name = "AWS Test"
+  }
+}
+
